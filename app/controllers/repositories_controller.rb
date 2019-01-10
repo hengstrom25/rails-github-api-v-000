6ebc2d8b1043c4a2a6ebebdbd49eeb5849385995
Heidi Engstrom <hengstrom25@gmail.com>
2019-01-10 18:28:02 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -2,9 +2,9 @@ class RepositoriesController < ApplicationController
   
   def index
     resp = Faraday.get("https://api.github.com/user") do |req|
-    req.params['oauth_token'] = session[:token]
-    # don't forget that pesky v param for versioning
-    req.params['v'] = '20160201'
+        req.headers['Authorization'] = 'token ' + session[:token]
+        req.headers['Accept'] = 'application/json'
+
   end
   
     @repos = JSON.parse(resp.body)

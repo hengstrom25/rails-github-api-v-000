52d4f6278bf675e9179700fa5c2a6c4b5688a088
Heidi Engstrom <hengstrom25@gmail.com>
2019-01-10 18:22:30 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,7 @@
 class RepositoriesController < ApplicationController
   
   def index
-    resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
+    resp = Faraday.get("https://api.github.com/user") do |req|
     req.params['oauth_token'] = session[:token]
     # don't forget that pesky v param for versioning
     req.params['v'] = '20160201'

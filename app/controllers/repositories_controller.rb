06346d0c0b8826c87ec08045437737b5c4231854
Heidi Engstrom <hengstrom25@gmail.com>
2019-01-10 18:29:34 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -4,8 +4,9 @@ class RepositoriesController < ApplicationController
     resp = Faraday.get("https://api.github.com/user") do |req|
         req.headers['Authorization'] = 'token ' + session[:token]
         req.headers['Accept'] = 'application/json'
-
-  end
+    end
+    
+    @user = 
   
     @repos = JSON.parse(resp.body)
   

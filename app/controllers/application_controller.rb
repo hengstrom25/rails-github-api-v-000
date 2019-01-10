af151631a4df85b837af5a3485f8533ad4b0ca47
Heidi Engstrom <hengstrom25@gmail.com>
2019-01-10 17:47:07 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   def authenticate_user
     client_id = ENV['GITHUB_CLIENT_ID']
     client_secret = ENV['GITHUB_CLIENT_SECRET']
-    foursquare_url = "https://foursquare.com/oauth2/authenticate?client_id=#{client_id}&response_type=code&redirect_uri=#{redirect_uri}"
+    foursquare_url = "https://github.com/login/oauth/authorize"
     redirect_to foursquare_url unless logged_in?
   end
 
